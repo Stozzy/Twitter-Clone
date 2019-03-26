742955ed17c69a9b3d6d054be80cc30532bb6efd
Stozzy <sagoldyn@gmail.com>
2019-03-26 06:10:42 -0400

    After reviewing NetNinja's JS DOM tutorial I redesigned the entire twitter clone to not use local storage.  It's easier to read and help reinforce concepts of DOM manip.
    Signed-off-by: Stozzy <sagoldyn@gmail.com>

--- script.js
+++ script.js
@@ -1,38 +1,35 @@
-const form = document.querySelector('form');
-const ul = document.querySelector('ul');
-const button = document.querySelector('button');
-const input = document.getElementById('item');
-let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+const listHistory = document.querySelector('#post-history ul');
 
-localStorage.setItem('items', JSON.stringify(itemsArray));
-const data = JSON.parse(localStorage.getItem('items'));
-
-const liMaker = (text) => {
-  let li = document.createElement('li');
-  li.textContent = text;
-  ul.prepend(li);
-}
-
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-  //added if conditional to prevent pushing empty strings
-  if (input.value != "") {
-    itemsArray.push(input.value);
-    localStorage.setItem('items', JSON.stringify(itemsArray));
-    liMaker(input.value);
-    input.value = "";
-  }
+//adding chrips
+const addForm = document.forms['add-post'];
+addForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    const value = addForm.querySelector('input[type="text"]').value;
+    
+    //create element
+    const li = document.createElement('li');
+    const previousPost = document.createElement('span');
+    const deletePost = document.createElement('button');
+    
+    //add content
+    previousPost.textContent = value;
+    deletePost.textContent = 'delete';
+        
+    //add CSS classes
+    previousPost.classList.add('previous-post');
+    deletePost.classList.add('delete-post');
+    
+    //appending children to li, then to parent ul
+    li.appendChild(previousPost);
+    li.appendChild(deletePost);
+    listHistory.prepend(li);
 });
 
-data.forEach((item) => {
-  liMaker(item);
-});
-
-
-button.addEventListener('click', function () {
-  localStorage.clear();
-  while (ul.firstChild) {
-    ul.removeChild(ul.firstChild);
-  }
-  itemsArray = [];
+//delete chirps - event bubbling
+listHistory.addEventListener('click', function(e) {
+    if (e.target.className == 'delete-post') {
+        const li = e.target.parentElement;
+        listHistory.removeChild(li);
+    }
 });
